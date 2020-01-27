a2ed4c82b7ed099e9a894db610be5440636e61da
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2020-01-27 22:46:14 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,7 +1,7 @@
 class CreateStudents < ActiveRecord::Migration[5.1]
   def change
-    create_table :students do |p|
-      p.string :name
+    create_table :students do |o|
+      o.string :name
     end
   end
 end
